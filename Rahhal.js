c39ef1987057024def7ce331a740651ba73ac2f7
Mohamed Abdellatif <153854292+tafa234@users.noreply.github.com>
2025-01-09 19:24:18 +0200

    Add files via upload

---
+++ Rahhal.js
@@ -0,0 +1,39 @@
+const searchInput = document.querySelector('.search input');
+const searchButton = document.querySelector('.search button');
+
+searchButton.addEventListener('click', () => {
+    const searchQuery = searchInput.value.trim();
+    if (searchQuery) {
+        alert(`Search for:${searchQuery}`);
+    } else {
+        alert('Please Enter a Word to Search');
+    }
+});
+
+const header = document.querySelector('header');
+let lastScrollY = window.scrollY; // حفظ مكان التمرير الأخير
+
+// التحقق من حالة التمرير
+window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
+
+    // تغيير الخلفية بناءً على مكان التمرير
+    if (currentScrollY > 50) {
+        header.classList.add('scrolled'); // إضافة خلفية عند التمرير
+    } else {
+        header.classList.remove('scrolled'); // إزالة الخلفية عند العودة لأعلى الصفحة
+    }
+
+    // التحكم في ظهور/اختفاء الـ header عند التمرير لأعلى أو لأسفل
+    if (currentScrollY > lastScrollY) {
+        // إذا كنا نمر للأسفل
+        header.classList.add('hide'); // إخفاء الـ header
+        header.classList.remove('show');
+    } else {
+        // إذا كنا نمر للأعلى
+        header.classList.add('show'); // إظهار الـ header
+        header.classList.remove('hide');
+    }
+
+    lastScrollY = currentScrollY; // تحديث آخر مكان للتمرير
+});
